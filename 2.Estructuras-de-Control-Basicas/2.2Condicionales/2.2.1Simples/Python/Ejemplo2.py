e313041f28a571dbe2901ffe94ee66eb5838eb19
Jhordan <jhordanjesus945@gmail.com>
2025-09-24 16:59:29 -0500

    Agregando los 2 Ejercicio de Condicionales simples en C# y Python

---
+++ 2.Estructuras-de-Control-Basicas/2.2Condicionales/2.2.1Simples/Python/Ejemplo2.py
@@ -0,0 +1,8 @@
+# Condicional simple: Verificar si una persona es mayor de edad
+
+# Solicitar la edad al usuario
+edad = int(input("Ingrese su edad: "))
+
+# Verificar si es mayor de edad
+if edad >= 18:
+    print("Eres mayor de edad.")

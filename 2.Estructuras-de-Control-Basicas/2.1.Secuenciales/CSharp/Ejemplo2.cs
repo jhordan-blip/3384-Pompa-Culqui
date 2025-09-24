20dc9b13dbbe8a9fbb12a0aec0ce4d7b34c3486f
Jhordan <jhordanjesus945@gmail.com>
2025-09-24 11:03:35 -0500

    Agregando 2er Ejemplo de Secuenciales en CSharp

---
+++ 2.Estructuras-de-Control-Basicas/2.1.Secuenciales/CSharp/Ejemplo2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Hallar el volumen del cilindro 
+             */
+
+            double Radio, Altura;
+            double volumen, pi;
+
+
+            //Inicio 
+
+            Console.WriteLine("Ingrese el radio del cilindro: ");
+            Radio = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la altura del cilindro: ");
+            Altura = double.Parse(Console.ReadLine());
+
+            //Proceso 
+            pi = 3.1415;
+            volumen = Math.PI * (Radio * Radio) * (Altura);
+
+            //Salida
+
+            Console.WriteLine("El volumen del cilindro es de: " + volumen);
+
+
+            Console.ReadKey();
+        }
+    }
+}
